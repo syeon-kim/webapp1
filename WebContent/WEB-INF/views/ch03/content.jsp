cadd307214564ad75d76cc538e6426c4f879da96
s.yeon <yeon.4future@gmail.com>
2021-01-14 10:28:47 +0900

    POST! GET!

--- WebContent/WEB-INF/views/ch03/content.jsp
+++ WebContent/WEB-INF/views/ch03/content.jsp
@@ -25,12 +25,36 @@
 				
 				<div class="content">
 					<div class="sector">
-						<h5> </h5>
+						<h5> GET방식 데이터 전달 </h5>
 						<div>
-							
+							<a class="btn btn-info  btn-sm" href="method1?param1=문자열&param2=5&param3=3.14&param4=true&param5=2021-01-14"> 데이터 전달
+							</a>
 						</div>
 					</div>					
+					<div class="sector">
+						<h5>POST방식 데이터 전달 </h5>
+						<div>
+							<form method="post" action="method2">
+								<input type="text" name="param1" value="문자열"/><br/>
+								<input type="number" name="param2" value="5"/><br/>
+								<input type="number" name="param3" value="3.14"/><br/>
+								<input type="checkbox" name="param4" checked/><br/>
+								<input type="date" name="param5" value="2020-12-25"/><br/>
+								<button class="btn btn-info btn-sm"> 데이터 전달 </button>
+								<!-- 기본값이 이미 잇음 -->
+							</form>
+						</div>
+					</div>
 					
+					<div class="sector">
+						<h5>디폴트 값 </h5>
+						<div>
+							<form method="post" action="method3">
+								<input type="text" name="param1" value="문자열"/><br/>
+								<button class="btn btn-info btn-sm"> 데이터 전달 </button>
+							</form>
+						</div>
+					</div>	
 				</div>
 			</div>
 			
--- src/main/java/com/mycompany/webapp/controller/Ch03Controller.java
+++ src/main/java/com/mycompany/webapp/controller/Ch03Controller.java
@@ -1,10 +1,14 @@
 package com.mycompany.webapp.controller;
 
+import java.util.Date;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/ch03")
@@ -17,5 +21,47 @@ public class Ch03Controller {
 		logger.info("실행");
 		return "ch03/content";
 	}
-
+	
+	@GetMapping("/method1")
+	public String method1(
+			String param1,int param2, double param3,
+			boolean param4, 
+			@DateTimeFormat(pattern="yyyy-MM-dd") Date param5) {//넘어오는거 다 문자취급함
+		logger.info("param1: " +param1);
+		logger.info("param2: " +param2);
+		logger.info("param3: " +param3);
+		logger.info("param4: " +param4);
+		logger.info("param5: " +param5);
+		
+		return "ch03/content";
+	}
+	
+	@RequestMapping("/method2")
+	public String method2(
+			@RequestParam("param1")  String arg1,
+			@RequestParam("param2") int arg2, 
+			double param3, boolean param4, 
+			@DateTimeFormat(pattern="yyyy-MM-dd") Date param5) {//넘어오는거 다 문자취급함
+		logger.info("param1: " +arg1);
+		logger.info("param2: " +arg2);
+		logger.info("param3: " +param3);
+		logger.info("param4: " +param4);
+		logger.info("param5: " +param5);
+		
+		return "ch03/content";
+	}
+	
+	@RequestMapping("/method3")
+	public String method3(
+			String param1, 
+			@RequestParam(defaultValue = "0") int param2, 
+			@RequestParam(defaultValue = "0.0") double param3, 
+			@RequestParam(defaultValue = "false") boolean param4 ) {
+		logger.info("param1: " + param1);
+		logger.info("param2: " + param2);
+		logger.info("param3: " + param3);
+		logger.info("param4: " + param4);
+		
+		return "ch03/content";
+	}
 }
